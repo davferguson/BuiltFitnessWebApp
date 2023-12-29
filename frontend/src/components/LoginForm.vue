0b94ffc7e5f1e5aa6fb50d84b793dcb89effd807
Davin Ferguson <davferg43@gmail.com>
2023-12-29 15:52:50 -0500

    updated favicon and title

--- frontend/src/components/LoginForm.vue
+++ frontend/src/components/LoginForm.vue
@@ -118,7 +118,7 @@ export default {
                 if(response.status == 200) {
                     this.$store.commit("SET_AUTH_TOKEN", response.data.token);
                     this.$store.commit("SET_USER", response.data.user);
-                    this.$router.push("/home");
+                    this.$router.push({ name: 'home' });
                 }
             })
             .catch(error => {
