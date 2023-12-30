b0a0add2f0ec395894413ab5f32bac9f698528d5
Davin Ferguson <davferg43@gmail.com>
2023-12-29 22:06:03 -0500

    new navbar

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -1,28 +1,39 @@
 <template>
-    <div class="container">
-        <!-- <div class="nav-logo">
-            <router-link :to="{ name: 'home'}">MedTracker</router-link>
-        </div> -->
-        <ul>
+    <div class="background">
+        <div class="container">
             <div class="nav-logo" @click="setActive(listItemOne, 1)">
-                <router-link :to="{ name: 'home'}">MedTracker</router-link>
+                <router-link :to="{ name: 'home'}"><img src="../../public/images/built-logo.png"></router-link>
             </div>
-            <div id="selection-box"></div>
-            <li id="list-item-one" @click="setActive($event.target, 1)" :class="{ active : activeElement == 1 }"><router-link :to="{ name: 'home'}">Home</router-link></li>
-            <li id="list-item-two" @click="setActive($event.target, 2)" :class="{ active : activeElement == 2 }"><router-link :to="{ name: 'about'}">About</router-link></li>
-            <li><LogoutButton /></li>
-        </ul>
+            <ul>
+                <!-- <div id="selection-box"></div> -->
+                <li id="list-item-one" @click="setActive($event.target, 1)" :class="{ active : activeElement == 1 }"><router-link :to="{ name: 'home'}">Home</router-link></li>
+                <li id="list-item-two" @click="setActive($event.target, 2)" :class="{ active : activeElement == 2 }"><router-link :to="{ name: 'about'}">About</router-link></li>
+                <li><router-link :to="{ name: 'about'}">Classes</router-link></li>
+                <li><router-link :to="{ name: 'about'}">Online Programs</router-link></li>
+                <li><router-link :to="{ name: 'about'}">Contact</router-link></li>
+                <!-- <li><LogoutButton /></li> -->
+            </ul>
+            <div class="dropdown-btn">
+                <p>Account</p>
+                <span class="material-symbols-outlined">expand_more</span>
+                <div class="dropdown-content">
+                    <router-link :to="{ name: 'login'}">Login</router-link>
+                    <router-link :to="{ name: 'login'}">Register</router-link>
+                </div>
+            </div>
+        </div>
     </div>
+    
 </template>
 
 <script>
-import { useRoute, useRouter } from 'vue-router'
-import LogoutButton from '@/components/LogoutButton.vue';
+// import { useRoute, useRouter } from 'vue-router'
+// import LogoutButton from '@/components/LogoutButton.vue';
 
 export default {
     name: 'NavBar',
     components: {
-        LogoutButton,
+        // LogoutButton,
     },
     props: {
     },
@@ -42,31 +53,113 @@ export default {
             this.activeElement = element;
         }
     },
-    async mounted() {
-        const route = useRoute()
-        const router = useRouter()
-        await router.isReady()
-        let listItem = null;
-        switch(route.name) {
-            case 'home':
-                listItem = document.getElementById("list-item-one");
-                this.setActive(listItem, 1);
-                break;
-            case 'about':
-                listItem = document.getElementById("list-item-two");
-                this.setActive(listItem, 2);
-                break;
-        }
+    // async mounted() {
+    //     const route = useRoute()
+    //     const router = useRouter()
+    //     await router.isReady()
+    //     let listItem = null;
+    //     switch(route.name) {
+    //         case 'home':
+    //             listItem = document.getElementById("list-item-one");
+    //             this.setActive(listItem, 1);
+    //             break;
+    //         case 'about':
+    //             listItem = document.getElementById("list-item-two");
+    //             this.setActive(listItem, 2);
+    //             break;
+    //     }
 
-        this.listItemOne = document.getElementById("list-item-one");
-    }
+    //     this.listItemOne = document.getElementById("list-item-one");
+    // }
 }
 </script>
 
 <style scoped>
 @import url('https://fonts.googleapis.com/css?family=Roboto');
-    .container {
-        background-color: #5161ce;
+@import url('https://fonts.googleapis.com/css2?family=Rowdies:wght@300&display=swap');
+.background {
+    background-color: #231f20;
+}
+.container {
+    display: flex;
+    flex-direction: row;
+    position: relative;
+    align-items: center;
+    block-size: 80px;
+    background-color: #231f20;
+    padding-right: 20px;
+    max-width: 1400px;
+    margin-left: auto;
+    margin-right: auto;
+    font-family: 'Rowdies', sans-serif;
+}
+.nav-logo {
+    inline-size: 260px;
+    flex-shrink: 0;
+}
+.nav-logo img {
+    width: 100%;
+}
+.dropdown-btn {
+    color: rgb(255, 255, 255);
+    font-size: 1.1rem;
+    display: flex;
+    align-items: center;
+    height: 100%;
+    cursor: pointer;
+}
+.dropdown-content {
+    display: none;
+    position: absolute;
+    top: 100%;
+    background-color: #ffffff;
+    box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+    min-width: 120px;
+    z-index: 1;
+}
+.dropdown-content a {
+    display: block;
+    color: #231f20;
+    padding-top: 15px;
+    height: 30px;
+    font-size: .9rem;
+    padding-left: 10px;
+    text-align: left;
+}
+.dropdown-btn:hover .dropdown-content {
+    display: block;
+}
+.dropdown-content a:hover {
+    background-color: #f0f0f0;
+}
+.dropdown-btn:hover {
+    color: #ffffffcc;
+}
+span {
+    font-size: 19px;
+    font-weight: bolder;
+    vertical-align: middle;
+}
+ul {
+    display: flex;
+    margin-left: auto;
+    margin-right: auto;
+    list-style: none;
+    font-size: 1.1rem;
+}
+li {
+    margin-left: 30px;
+    margin-right: 30px;
+}
+a {
+    text-decoration: none;
+    color: #ffffff;
+}
+li a:hover {
+    color: #ffffffcc;
+}
+    /* .container {
+        background-color: #000000;
         float: left;
         overflow: hidden;
         position: relative;
@@ -119,5 +212,5 @@ export default {
     }
     ul {
     text-align: right;
-    }
+    } */
 </style>
