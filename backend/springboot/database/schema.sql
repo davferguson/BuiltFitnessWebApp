dd2d35d642373f24221e8b60bf37b5b1bf4caccc
Davin Ferguson <davferg43@gmail.com>
2024-02-26 19:25:50 -0500

    added courses table to database

--- backend/springboot/database/schema.sql
+++ backend/springboot/database/schema.sql
@@ -1,6 +1,7 @@
 BEGIN TRANSACTION;
 
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS courses;
 
 CREATE TABLE IF NOT EXISTS users
 (
@@ -14,5 +15,16 @@ CREATE TABLE IF NOT EXISTS users
     CONSTRAINT pk_user PRIMARY KEY (user_id),
     CONSTRAINT users_username_key UNIQUE (username)
 );
+
+CREATE TABLE IF NOT EXISTS courses
+(
+    course_id SERIAL,
+    course_name VARCHAR(50) NOT NULL,
+    image_key VARCHAR(50) NOT NULL,
+    description VARCHAR[],
+    price DECIMAL NOT NULL,
+    UNIQUE(course_name),
+    CONSTRAINT pk_course PRIMARY KEY (course_id)
+)
 
 COMMIT TRANSACTION;
