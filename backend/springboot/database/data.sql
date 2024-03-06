a5f53778ad83e671d653645c2e13872e3efd174e
Davin Ferguson <davferg43@gmail.com>
2024-03-06 11:14:44 -0500

    added roles to database data

--- backend/springboot/database/data.sql
+++ backend/springboot/database/data.sql
@@ -1,5 +1,11 @@
 BEGIN TRANSACTION;
 
+INSERT INTO custom_role (role_name)
+VALUES('ADMIN');
+
+INSERT INTO custom_role (role_name)
+VALUES('DEFAULT');
+
 INSERT INTO courses (course_name, image_key, description, price)
 VALUES('Ab Trainer', 'abtrainer', ARRAY ['ajld jkwdlod daskl okda', 'njkdaq ajksod nnw', 'ndw dasd dednasd ad'], 19.99);
 
