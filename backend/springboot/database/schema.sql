bfd9fd8540fcd45a1027c19da58220a57639bb92
Davin Ferguson <davferg43@gmail.com>
2024-03-06 11:10:37 -0500

    added custom_role and custom_role_user tables to database

--- backend/springboot/database/schema.sql
+++ backend/springboot/database/schema.sql
@@ -1,6 +1,8 @@
 BEGIN TRANSACTION;
 
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS custom_role;
+DROP TABLE IF EXISTS custom_role_user;
 DROP TABLE IF EXISTS courses;
 
 CREATE TABLE IF NOT EXISTS users
@@ -14,6 +16,19 @@ CREATE TABLE IF NOT EXISTS users
     status smallint DEFAULT 1,
     CONSTRAINT pk_user PRIMARY KEY (user_id),
     CONSTRAINT users_username_key UNIQUE (username)
+);
+
+CREATE TABLE IF NOT EXISTS custom_role (
+	role_id SERIAL,
+	role_name varchar(50) NOT NULL,
+	CONSTRAINT PK_role PRIMARY KEY (role_id)
+);
+
+CREATE TABLE IF NOT EXISTS custom_role_user (
+	role_id INT NOT NULL,
+	user_id INT NOT NULL,
+	CONSTRAINT FK_role_id FOREIGN KEY (role_id) REFERENCES custom_role (role_id),
+	CONSTRAINT FK_user_id FOREIGN KEY (user_id) REFERENCES users (user_id)
 );
 
 CREATE TABLE IF NOT EXISTS courses
