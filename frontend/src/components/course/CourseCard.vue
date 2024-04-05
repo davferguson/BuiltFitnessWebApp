f4014687dab04f251e1e6d4e325ee0911dfd5811
Davin Ferguson <davferg43@gmail.com>
2024-04-05 17:45:10 -0400

    created a CourseCard component for front-end

---
+++ frontend/src/components/course/CourseCard.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h3>Course Info</h3>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'course-card',
+    props: {
+        course: Object,
+    },
+    data() {
+        return {
+            
+        }
+    }
+}
+</script>
